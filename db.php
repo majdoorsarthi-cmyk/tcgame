2b756842768b2ee95431889262edb2f86c72aa7e
majdoorsarthi-cmyk <majdoorsarthi@gmail.com>
2026-09-14 08:58:42 +0530

    Update db.php

--- db.php
+++ db.php
@@ -3,13 +3,12 @@ $host = "mysql-9d9cc53-majdoorsarthi-d1a8.k.aivencloud.com";
 $port = 13848;
 $username = "avnadmin";
 $password = "AVNS_CXh977fYw0GUSdyTCUU";
-$dbname = "defaultdb";
+$dbname = "tcgame"; // updated to tcgame database
 
 $conn = mysqli_init();
 mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
-mysqli_real_connect($conn, $host, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL);
 
-if (!$conn) {
+if (!@mysqli_real_connect($conn, $host, $username, $password, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
